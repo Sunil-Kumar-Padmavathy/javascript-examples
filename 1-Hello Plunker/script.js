60cf62fec67c59c615df46dd5d6ba328e88b6fc3
Sunil Kumar Padmavathy <SunilKumar.Padmavathy@gmail.com>
2018-04-09 22:46:42 -0400

    Hello Plunker and console examples

---
+++ 1-Hello Plunker/script.js
@@ -0,0 +1,11 @@
+let firstName ="John";
+let lastName=" Doe";
+
+console.log(firstName+lastName);
+console.log(firstName,lastName);
+
+let firstName1 ="John1",
+    lastName1=" Doe1";
+
+console.log(firstName1,lastName1);
+
