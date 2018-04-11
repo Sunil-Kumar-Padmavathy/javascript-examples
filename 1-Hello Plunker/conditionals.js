24cdcd8467c8f7a817f4958a4911c727c1aff931
Sunil Kumar Padmavathy <SunilKumar.Padmavathy@gmail.com>
2018-04-10 23:45:18 -0400

    included conditionals

---
+++ 1-Hello Plunker/conditionals.js
@@ -0,0 +1,23 @@
+if(1 <2){
+  console.log("1 is lesser than 2");
+}
+
+if(2 == 2){
+  console.log("2 is equal to 2")
+}
+
+if(2 === 2){
+  console.log("2 is equal to 2")
+}
+
+if(2<=1){
+  console.log("doesn't execute")
+}
+
+let fruit= "Orange"
+let price =0;
+ if(fruit == "Orange"){
+   price=3;
+ }
+ 
+ console.log(price);
