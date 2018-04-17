6d716a36d5fc4ced5c4d59d03774e44fad0ea538
Sunil Kumar Padmavathy <SunilKumar.Padmavathy@gmail.com>
2018-04-16 23:07:47 -0400

    DOM- event handler example

--- 2-DOM/script.js
+++ 2-DOM/script.js
@@ -1,13 +1,39 @@
 
+
+
 function changeText(){
 let textArea = document.getElementById('paragraph');
 if(textArea.innerText=='Updated Paragraph Text'){
+	textArea.style.display='block';
 	textArea.innerText = 'Testing DOM'; 
+	
 }
 
 else 
 {
+	textArea.style.display='block';
 	textArea.innerText = 'Updated Paragraph Text';
 }
 
-}
+};
+
+
+
+function triggerEvent(){
+	
+	let eventButton=document.getElementById('eventButton');
+	let textArea = document.getElementById('paragraph');
+	
+	eventButton.addEventListener('click',function(){
+		
+		textArea.style.display='none';
+
+	});
+		
+};
+
+
+
+
+
+
